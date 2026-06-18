34073fea28b0ffc17555373770bc25757e19cdf9
Revanth Kumar <sagala.revanthkumar955@gmail.com>
2026-06-18 18:14:51 +0100

    template finalised and added setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Emotion_detection"
+AUTHOR_USER_NAME = "Revanth"
+SRC_REPO = "emotion_detection"
+AUTHOR_EMAIL = "sagala.revanthkumar955@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="Implementing Deep learning to detect emotions in real-time",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Summariser": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)
